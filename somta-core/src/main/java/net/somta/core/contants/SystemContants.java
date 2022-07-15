960f1b8129bab37259260ffef5d526a2e8e98876
husong <305123882@qq.com>
2022-07-15 18:05:46 +0800

    fix: 去掉container项目，移动到web上

--- somta-core/src/main/java/net/somta/core/contants/SystemContants.java
+++ somta-core/src/main/java/net/somta/core/contants/SystemContants.java
@@ -1,5 +1,17 @@
 package net.somta.core.contants;
 
+/**
+ * 系统统一常量
+ */
 public class SystemContants {
+
+    /**
+     * 框架底层统一前缀
+     */
     public final static int BASE_CODE_PREFIX = 100;
+
+    /**
+     * 缓存starter的code
+     */
+    public final static long STATER_CACHE_CODE = (BASE_CODE_PREFIX * 1000 + 100) * 10000;
 }
