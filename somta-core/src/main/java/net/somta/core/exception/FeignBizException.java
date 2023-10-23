da3bc1d4ea67686aff6b533f98c1deffdfe802c5
husong <305123882@qq.com>
2023-10-23 20:28:35 +0800

    feat:移动feignBizException到core

---
+++ somta-core/src/main/java/net/somta/core/exception/FeignBizException.java
@@ -0,0 +1,36 @@
+package net.somta.core.exception;
+
+import feign.FeignException;
+import feign.Request;
+import feign.Util;
+
+/**
+ * 自定义openfeign的异常
+ * @author husong
+ * @date 2022/08/31
+ */
+public class FeignBizException extends FeignException {
+
+    private long errorCode;
+    private String errorMsg;
+
+    public FeignBizException(long errorCode, int status, String message, Request request) {
+        super(status, (String) Util.checkNotNull(message, "message", new Object[0]), request);
+        this.errorCode = errorCode;
+        this.errorMsg = message;
+    }
+
+    public FeignBizException(long errorCode, int status, String message, Request request, Throwable cause) {
+        super(status, message, request, (Throwable)Util.checkNotNull(cause, "cause", new Object[0]));
+        this.errorCode = errorCode;
+        this.errorMsg = message;
+    }
+
+    public long getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+}
