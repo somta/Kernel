cbe2d1bf9d77bc9b6e55072d087b7f0b5ad6be52
husong <305123882@qq.com>
2025-08-05 19:03:37 +0800

    feat:添加单元测试

---
+++ kernel-extra/src/test/java/net/somta/extra/cache/redis/client/AbstractRedisClientTest.java
@@ -0,0 +1,44 @@
+package net.somta.extra.cache.redis.client;
+
+import net.somta.extra.cache.redis.RedisClientBuilder;
+import net.somta.extra.cache.redis.model.RedisConfigItem;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RScript;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class AbstractRedisClientTest {
+
+    @Test
+    public void evalSimpleLuaTest() throws IOException {
+        RedisConfigItem redisConfigItem = new RedisConfigItem();
+        String[] address = new String[]{"127.0.0.1:6379"};
+        redisConfigItem.setAddress(address);
+        AbstractRedisClient redisClient = RedisClientBuilder.buildRedisClient(redisConfigItem);
+        RedissonClient redissonClient = redisClient.getRedissonClient();
+        RScript script = redissonClient.getScript(StringCodec.INSTANCE);
+        Object result = script.eval(RScript.Mode.READ_WRITE, "return redis.call('get', 'test')", RScript.ReturnType.VALUE);
+        System.out.println(result);
+    }
+
+    @Test
+    public void evalLuaTest() throws IOException {
+        RedisConfigItem redisConfigItem = new RedisConfigItem();
+        String[] address = new String[]{"127.0.0.1:6379"};
+        redisConfigItem.setAddress(address);
+        AbstractRedisClient redisClient = RedisClientBuilder.buildRedisClient(redisConfigItem);
+        RedissonClient redissonClient = redisClient.getRedissonClient();
+        RScript script = redissonClient.getScript(StringCodec.INSTANCE);
+        String luaScript = """
+                redis.call('set','name','hs');
+                return redis.call('get', 'test');
+                """;
+        // 加载脚本
+        String sha1 = script.scriptLoad(luaScript);
+        Object result = script.evalSha(RScript.Mode.READ_WRITE, sha1, RScript.ReturnType.VALUE);
+        System.out.println(result);
+    }
+}
