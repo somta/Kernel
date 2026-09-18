477c6e693c1461e27dffab23d051223ca423775c
明天的地平线 <305123882@qq.com>
2026-09-19 01:48:44 +0800

    fix:优化代码

--- kernel-common/src/main/java/com/matecoder/common/utils/MapUtil.java
+++ kernel-common/src/main/java/com/matecoder/common/utils/MapUtil.java
@@ -75,48 +75,9 @@ public class MapUtil {
     public static void removeNullValue(Map map){
         Set set = map.keySet();
         for (Iterator iterator = set.iterator(); iterator.hasNext();) {
-            Object obj = (Object) iterator.next();
-            Object value =(Object)map.get(obj);
-            remove(value, iterator);
-        }
-    }
-
-    /**
-     * 移除map中的空值
-     *
-     * Iterator 是工作在一个独立的线程中，并且拥有一个 mutex 锁。
-     * Iterator 被创建之后会建立一个指向原来对象的单链索引表，当原来的对象数量发生变化时，这个索引表的内容不会同步改变，
-     * 所以当索引指针往后移动的时候就找不到要迭代的对象，所以按照 fail-fast 原则 Iterator 会马上抛出 java.util.ConcurrentModificationException 异常。
-     * 所以 Iterator 在工作的时候是不允许被迭代的对象被改变的。但你可以使用 Iterator 本身的方法 remove() 来删除对象， Iterator.remove() 方法会在删除当前迭代对象的同时维护索引的一致性。
-     * @param obj object instance
-     * @param iterator iterator
-     */
-    private static void remove(Object obj, Iterator iterator){
-        if(obj instanceof String){
-            String str = (String)obj;
-            if(StringUtils.isEmpty(str)){
-                iterator.remove();
-            }
-
-        }else if(obj instanceof Collection){
-            Collection col = (Collection)obj;
-            if(col==null||col.isEmpty()){
-                iterator.remove();
-            }
-
-        }else if(obj instanceof Map){
-            Map temp = (Map)obj;
-            if(temp==null||temp.isEmpty()){
-                iterator.remove();
-            }
-
-        }else if(obj instanceof Object[]){
-            Object[] array =(Object[])obj;
-            if(array==null||array.length<=0){
-                iterator.remove();
-            }
-        }else{
-            if(obj==null){
+            Object obj = iterator.next();
+            Object value = map.get(obj);
+            if(value == null || (value instanceof String && StringUtils.isEmpty((String) value))){
                 iterator.remove();
             }
         }
